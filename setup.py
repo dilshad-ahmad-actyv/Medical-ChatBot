c1e2fdf9c4f1aa6b3d2c5b85baaabd94e79a75dd
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2024-06-04 00:31:21 +0530

    added setup tools in the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical Chatbot",
+    version = '0.0.0',
+    author="Dilshad Ahmad",
+    author_email="dilshadsoraon09@gmail.com",
+    packages=find_packages(),
+    requires=[]
+)
